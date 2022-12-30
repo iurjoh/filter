495f83041a256d9360817a514cc3271aa8cb22f4
Iuri Johansson <iuri_abreu@hotmail.com>
2022-12-30 12:14:19 +0000

    explanations about the topic and examples

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === "Paul")[0];
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,18 @@ const students = [
     ]
   },
 ];
+
+// complex but descriptive method
+
+// const candidates = students.filter(student => {
+//   let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+//   return strongSkills.length > 0;
+// });
+// console.log(candidates);
+
+
+// simple and cleaner method
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
